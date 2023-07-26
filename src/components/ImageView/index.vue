9025b367f46f0cf570c5157d6be8f02695a1b569
孤风作伴 流雨为景 <8550325+p785356903@user.noreply.gitee.com>
2023-07-26 15:34:14 +0800

    放大镜效果-滑块跟随鼠标移动

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -1,7 +1,8 @@
 <!-- @format -->
 
 <script setup>
-import { ref } from 'vue';
+import { useMouseInElement } from '@vueuse/core';
+import { ref, watch } from 'vue';
 
 // 图片列表
 const imageList = [
@@ -16,6 +17,52 @@ const activeIndex = ref(0);
 const enterhandler = i => {
   activeIndex.value = i;
 };
+
+// 鼠标移动
+// 监听DOM元素
+/* 
+  target
+  绑定的DOM元素中鼠标的X和Y值
+  elementX , elementY
+  鼠标是否在绑定的DOM元素中
+  isOutside false 是在DOM元素中,true是不在DOM元素中 
+*/
+// 2. 获取鼠标相对位置
+const target = ref(null);
+const { elementX, elementY, isOutside } = useMouseInElement(target);
+
+// 3. 控制滑块跟随鼠标移动（监听elementX/Y变化，一旦变化 重新设置left/top）
+const left = ref(0);
+const top = ref(0);
+
+const positionX = ref(0);
+const positionY = ref(0);
+
+watch([elementX, elementY, isOutside], () => {
+  // console.log(elementX, elementY, isOutside);
+  // 如果鼠标没有移入到盒子里面 直接不执行后面的逻辑
+  if (isOutside.value) return;
+
+  // 处理边界
+  if (elementX.value < 100) left.value = 0;
+  if (elementX.value > 300) left.value = 200;
+
+  if (elementY.value < 100) top.value = 0;
+  if (elementY.value > 300) top.value = 200;
+  // 有效范围内控制滑块距离
+  // 横向
+  if (elementX.value > 100 && elementX.value < 300) {
+    left.value = elementX.value - 100;
+  }
+  // 纵向
+  if (elementY.value > 100 && elementY.value < 300) {
+    top.value = elementY.value - 100;
+  }
+
+  // 控制大图的显示
+  positionX.value = -left.value * 2;
+  positionY.value = -top.value * 2;
+});
 </script>
 
 <template>
@@ -24,7 +71,7 @@ const enterhandler = i => {
     <div class="middle" ref="target">
       <img :src="imageList[activeIndex]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `0px`, top: `0px` }"></div>
+      <div class="layer" v-show="!isOutside" :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
@@ -41,12 +88,12 @@ const enterhandler = i => {
       class="large"
       :style="[
         {
-          backgroundImage: `url(${imageList[0]})`,
-          backgroundPositionX: `0px`,
-          backgroundPositionY: `0px`,
+          backgroundImage: `url(${imageList[activeIndex]})`,
+          backgroundPositionX: `${positionX}px`,
+          backgroundPositionY: `${positionY}px`,
         },
       ]"
-      v-show="false"></div>
+      v-show="!isOutside"></div>
   </div>
 </template>
 
